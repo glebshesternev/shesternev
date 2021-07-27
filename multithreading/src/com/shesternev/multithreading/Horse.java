89e42a9d1c6b25b1c6da18c00ddd6507a0b5a1a5
glebshesternev <cool.gleb3349920@gmail.com>
2021-07-27 19:14:20 +0300

    add maven add lombok refactor Horse

--- multithreading/src/com/shesternev/multithreading/Horse.java
+++ multithreading/src/com/shesternev/multithreading/Horse.java
@@ -1,6 +1,9 @@
 package com.shesternev.multithreading;
 
-public class Horse {
+
+import lombok.Data;
+
+public @Data class Horse {
     private String name;
     private int speed;
     private long distance;
@@ -8,30 +11,6 @@ public class Horse {
     public Horse(String name, int speed, long distance) {
         this.name = name;
         this.speed = speed;
-        this.distance = distance;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
-
-    public long getDistance() {
-        return distance;
-    }
-
-    public void setDistance(long distance) {
         this.distance = distance;
     }
 
