1326bc18b72ee537aa210502b4eb146ebaebe81d
glebshesternev <cool.gleb3349920@gmail.com>
2021-07-28 23:38:45 +0300

    move Horse.class in recourses to load class with package rename classloader module

--- shclassloader/src/main/java/com/shesternev/classloader/ModuleLoader.java
+++ shclassloader/src/main/java/com/shesternev/classloader/ModuleLoader.java
@@ -7,7 +7,7 @@ import java.lang.reflect.Method;
 public class ModuleLoader {
     public static void main(String[] args) throws ClassNotFoundException {
         MyClassLoader classLoader = new MyClassLoader("", ClassLoader.getSystemClassLoader());
-        Class<?> clazz = classLoader.loadClass("MyArrayList");
+        Class<?> clazz = classLoader.loadClass("com.shesternev.multithreading.model.Horse");
         System.out.println(clazz.getName());
         for (Method method : clazz.getMethods()) {
             System.out.println(method.getName());
