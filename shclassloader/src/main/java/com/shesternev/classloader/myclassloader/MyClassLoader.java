76d0a9bbd1c460088d562b5ec8c516185c850837
glebshesternev <cool.gleb3349920@gmail.com>
2021-07-29 13:28:19 +0300

    refactor MyClassLoader

--- shclassloader/src/main/java/com/shesternev/classloader/myclassloader/MyClassLoader.java
+++ shclassloader/src/main/java/com/shesternev/classloader/myclassloader/MyClassLoader.java
@@ -1,7 +1,11 @@
 package com.shesternev.classloader.myclassloader;
 
-import java.io.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileNotFoundException;
 
 public class MyClassLoader extends ClassLoader {
     private final String path;
@@ -15,25 +19,25 @@ public class MyClassLoader extends ClassLoader {
     @Override
     public Class<?> findClass(String className) throws ClassNotFoundException {
         try {
-            byte[] bytes = fetchClassFromFS(path + className + ".class");
+            byte[] bytes = fetchClassFromFileStream(path + className + ".class");
             return defineClass(className, bytes, 0, bytes.length);
         } catch (IOException e) {
             return super.findClass(className);
         }
     }
 
-    private byte[] fetchClassFromFS(String path) throws IOException {
+    private byte[] fetchClassFromFileStream(String path) throws IOException {
 
         long length = new File(path).length();
         byte[] bytes = new byte[(int) length];
-        try (InputStream is = new FileInputStream(path)) {
+        try (InputStream inputStream = new FileInputStream(path)) {
             if (length > Integer.MAX_VALUE) {
                 throw new FileNotFoundException("File " + path + " not found");
             }
             int offset = 0;
             int numRead = 0;
             while (offset < bytes.length && numRead >= 0) {
-                numRead = is.read(bytes, offset, bytes.length - offset);
+                numRead = inputStream.read(bytes, offset, bytes.length - offset);
                 offset += numRead;
             }
             if (offset < bytes.length) {
