339a50df488d3be0540ec35317c6f365f9c641d4
glebshesternev <cool.gleb3349920@gmail.com>
2021-07-29 13:11:26 +0300

    add try with resources

--- shclassloader/src/main/java/com/shesternev/classloader/myclassloader/MyClassLoader.java
+++ shclassloader/src/main/java/com/shesternev/classloader/myclassloader/MyClassLoader.java
@@ -23,24 +23,23 @@ public class MyClassLoader extends ClassLoader {
     }
 
     private byte[] fetchClassFromFS(String path) throws IOException {
-        InputStream is = new FileInputStream(path);
+
         long length = new File(path).length();
-        if (length > Integer.MAX_VALUE) {
-            is.close();
-            throw new FileNotFoundException("File " + path + " not found");
-        }
         byte[] bytes = new byte[(int) length];
-        int offset = 0;
-        int numRead = 0;
-        while (offset < bytes.length && numRead >= 0) {
-            numRead = is.read(bytes, offset, bytes.length - offset);
-            offset += numRead;
+        try (InputStream is = new FileInputStream(path)) {
+            if (length > Integer.MAX_VALUE) {
+                throw new FileNotFoundException("File " + path + " not found");
+            }
+            int offset = 0;
+            int numRead = 0;
+            while (offset < bytes.length && numRead >= 0) {
+                numRead = is.read(bytes, offset, bytes.length - offset);
+                offset += numRead;
+            }
+            if (offset < bytes.length) {
+                throw new IOException("Could not read file " + path);
+            }
         }
-        if (offset < bytes.length) {
-            is.close();
-            throw new IOException("Could not read file " + path);
-        }
-        is.close();
         return bytes;
     }
 }
