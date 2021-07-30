92c4f6f3cfbfe2c0c03b920b692b21bf7d6e6014
glebshesternev <cool.gleb3349920@gmail.com>
2021-07-30 18:59:53 +0300

    init flyway

---
+++ shdb/src/main/java/com/shesternev/db/SpringBootApp.java
@@ -0,0 +1,21 @@
+package com.shesternev.db;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class SpringBootApp implements CommandLineRunner {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApp.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        log.info("db init");
+    }
+}
---
+++ shdb/src/main/resources/db/migration/V1_0__CREATE_USERS.sql
@@ -0,0 +1,7 @@
+create table users (
+                       id int,
+                       name varchar(20) NOT NULL,
+                       email varchar(50) NOT NULL,
+                       password varchar(15) NOT NULL,
+                       PRIMARY KEY (id)
+);
