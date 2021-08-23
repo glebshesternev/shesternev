88a41b3e55abb5a9e012de49ee221c8a354293f8
glebshesternev <cool.gleb3349920@gmail.com>
2021-08-23 13:55:36 +0300

    refactor ...

--- shcache/src/test/java/com/shesternev/cache/CacheTest.java
+++ shcache/src/test/java/com/shesternev/cache/CacheTest.java
@@ -13,7 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Config.class)
+@SpringBootTest(classes = TestConfig.class)
 public class CacheTest {
 
     @Autowired
--- shcache/src/test/java/com/shesternev/cache/TestConfig.java
+++ shcache/src/test/java/com/shesternev/cache/TestConfig.java
@@ -14,7 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @TestConfiguration
 @EnableScheduling
-public class Config {
+public class TestConfig {
 
     @Value("${user.count}")
     private int userCount;
